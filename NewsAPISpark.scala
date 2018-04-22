0a05b834a5b51034faede4ebaa69054e076edb0d
Jay Lohokare <jaylohokare@gmail.com>
2018-04-22 00:43:31 -0400

    Added extract all htmls and save data to cassandra

--- NewsAPISpark.scala
+++ NewsAPISpark.scala
@@ -10,6 +10,8 @@ z.load("/home/jay/Desktop/spark-2.3.0-bin-hadoop2.7/jars/tika-core-1.17.jar")
 z.load("/home/jay/Desktop/spark-2.3.0-bin-hadoop2.7/jars/tika-app-1.17.jar")
 z.load("/home/jay/Desktop/spark-2.3.0-bin-hadoop2.7/jars/tika-bundle-1.17.jar")
 z.load("/home/jay/Desktop/spark-2.3.0-bin-hadoop2.7/jars/tika-parsers-1.17.jar")
+z.load("datastax:spark-cassandra-connector:2.0.7-s_2.10")
+
 
 
 
@@ -36,35 +38,76 @@ import java.net.URL;
 val googleNewsURL = "https://newsapi.org/v2/everything"
 //This can be any URL to make the REST API call
 
+
 val df = spark
   .read
   .format("org.apache.spark.sql.cassandra")
   .options(Map( "table" -> "cryptonews", "keyspace" -> "uptick" ))
   .load()
-//Loading parameters from Cassandra
   
 df.show
 
-df.createOrReplaceTempView("GoogleNewAPIData")
-val parmg = Map("url" -> googleNewsURL, "input" -> "GoogleNewAPIData", "method" -> "GET", "readTimeout" -> "10000", "connectionTimeout" -> "2000", "partitions" -> "10")
-val newsDataDf = spark.read.format("org.apache.dsext.spark.datasource.rest.RestDataSource").options(parmg).load()
-newsDataDf.printSchema 
-newsDataDf.createOrReplaceTempView("GoogleNewTempView")
+df.createOrReplaceTempView("GoogleNewAPIData");
+val parmg = Map("url" -> googleNewsURL, "input" -> "GoogleNewAPIData", "method" -> "GET", "readTimeout" -> "10000", "connectionTimeout" -> "2000", "partitions" -> "10");
+val newsDataDf = spark.read.format("org.apache.dsext.spark.datasource.rest.RestDataSource").options(parmg).load();
+newsDataDf.createOrReplaceTempView("GoogleNewTempView");
+var outDF = spark.sql("select output.articles from GoogleNewTempView");
 
-val outDF = spark.sql("select output.articles.url from GoogleNewTempView")
 
 /*******************************News API call**********************************/
 
 
 
-/*******************************Apache Tika to extract text from htmls fetched**********************************/
-//Run this part over the data fetched from NewsApi.org
-var stream : InputStream = new URL("http://jaylohokare.com/index.html").openStream()
-val handler : BodyContentHandler = new BodyContentHandler()
-val metadata : Metadata = new Metadata()
-val context : ParseContext = new ParseContext()
-val htmlparser : HtmlParser = new HtmlParser()
-htmlparser.parse(stream, handler, metadata, context);
-println(handler.toString())
+/*******************************Apache Tika to extract text from htmls fetched and save to Cassandra**********************************/
+def ParseURLContent(url: String) : String = {
+
+    try { 
+        var stream : InputStream = new URL(url).openStream()
+        val handler : BodyContentHandler = new BodyContentHandler()
+        val metadata : Metadata = new Metadata()
+        val context : ParseContext = new ParseContext()
+        val htmlparser : HtmlParser = new HtmlParser()
+        htmlparser.parse(stream, handler, metadata, context)
+        var content = handler.toString().trim().replaceAll(" +", " ").replaceAll("\n", " ").replaceAll("\\s", " ").replaceAll("\\s+", " ").filter(_ >= ' ').replaceAll("""(?m)\s+$""","").replaceAll("""^\s+(?m)""","").replaceAll("\\P{Print}", "").replaceAll("\\\\x\\p{XDigit}{2}", "")
+        return content
+        
+   } catch {
+        case e: Exception => return "ERROR FETCHING"
+   }
+}
+
+def RemoveDescriptionSpaces(description: String) : String = {
+    return description.toString().trim().replaceAll(" +", " ").replaceAll("\n", " ").replaceAll("\\s", " ").replaceAll("\\s+", " ").filter(_ >= ' ').replaceAll("""(?m)\s+$""","").replaceAll("""^\s+(?m)""","").replaceAll("\\P{Print}", "").replaceAll("\\\\x\\p{XDigit}{2}", "")
+}
+
+
+var explodedOut = outDF.select(explode(outDF("articles")))
+
+import org.apache.spark.sql.functions._
+val transformFunctiom = udf(ParseURLContent _)
+val transformFunctiom2 = udf(RemoveDescriptionSpaces _)
+
+val finalDataFrame = explodedOut.
+withColumn("author", explodedOut("col.author")).
+withColumn("description_notClean", explodedOut("col.description")).
+withColumn("publishedat", explodedOut("col.publishedAt")).
+withColumn("source_id", explodedOut("col.source.id")).
+withColumn("source_name", explodedOut("col.source.name")).
+withColumn("title", explodedOut("col.title")).
+withColumn("url", explodedOut("col.url")).
+withColumn("urltoimage", explodedOut("col.urlToImage")).
+withColumn("content", transformFunctiom($"url")).
+withColumn("description", transformFunctiom2($"description_notClean")).
+drop("col").
+drop("description_notClean")
+
+// finalDataFrame.show()
+
+finalDataFrame.write
+  .format("org.apache.spark.sql.cassandra")
+  .options(Map("table" -> "mynewsapi", "keyspace" -> "uptick"))
+  .save()
+
+
 
 /*******************************Apache Tika to extract text from htmls fetched**********************************/
